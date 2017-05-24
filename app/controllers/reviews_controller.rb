2ee0308173874ebc70e925e1105e2da9e383c671
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-05-24 17:07:19 +0100

    All links working, design in progress

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,29 @@
 class ReviewsController < ApplicationController
-
+  before_action :set_restaurant
   before_action :review_params, only: [:create]
 
   def index
-    @reviews = Review.all
+    @reviews = @restaurant.reviews
+    @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @review.restaurant = Restaurant.find(params[:restaurant_id])
-    @review.save
-    redirect_to restaurant_path(@review.restaurant)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render "restaurants/show"
+    end
   end
 
   private
 
   def review_params
     params.require(:review).permit(:rating, :content)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
