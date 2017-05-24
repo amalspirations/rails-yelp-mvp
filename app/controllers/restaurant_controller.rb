63fc9e4ab9b2b5779fdef61fba2a8d6cca1f28eb
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-05-24 11:24:21 +0100

    Prepare rails app with external specs

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,30 @@
+class RestaurantController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def params
+  end
+
+  def set_restaurant
+  end
+end
