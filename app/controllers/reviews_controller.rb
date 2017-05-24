ddd818de66edd07d0ed49c5dc0dfc0ae41fb323d
Amal Ketata <amal.ketata@edu.escpeurope.eu>
2017-05-24 15:33:58 +0100

    Functional app, improved version

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,11 +6,6 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
